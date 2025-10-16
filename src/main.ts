a5172259ce1ebdf613b5fcac6e40ec973605c2b5
Uri Chandler <uri@ironblocks.com>
2025-10-16 10:32:28 +0300

    chore: start web server

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-// import { ConfigService } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createApplicationContext(AppModule);
-
+  const app = await NestFactory.create(AppModule);
   app.enableShutdownHooks();
+
+  const configService = app.get<ConfigService>(ConfigService);
+  const PORT = configService.get<number>('PORT') as number;
+
+  await app.listen(PORT);
 }
 void bootstrap().catch(console.error);
